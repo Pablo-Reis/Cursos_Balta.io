2d8b1bdaa52e7b1c2a5b77319f7e612a0470c725
Pablo Leite <pablo16072002@gmail.com>
2023-02-17 20:38:22 -0300

    Update 17/02/2023

--- Desafios_Carnaval_Balta/Desafio1/Program.cs
+++ Desafios_Carnaval_Balta/Desafio1/Program.cs
@@ -3,9 +3,9 @@
     Console.Clear();
     Console.WriteLine("***********DESAFIO 01 - CARNAVAL BALTA.IO***********\n");
     Console.WriteLine("""Digite um texto (Para sair, basta digitar "exit"): """);
-    var inputText = Console.ReadLine();
+    var inputText = Console.ReadLine().Trim();
     if (inputText.ToLower() == "exit") Environment.Exit(0);
-    (int letterCount, int wordCount) = (inputText!.Length, inputText.Split(" ").Count());
+    (int letterCount, int wordCount) = (inputText!.Length, inputText.Split(' ').Count());
     Console.WriteLine($"{letterCount} caracteres, {wordCount} palavras");
     Console.ReadKey();
 }
