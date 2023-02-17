707d3d1740fbfb6d50e333b1695da20a84cedaac
Pablo Leite <pablo16072002@gmail.com>
2023-02-17 20:31:11 -0300

    Desafio Balta 17/02/2023

---
+++ Desafios_Carnaval_Balta/Desafio1/Program.cs
@@ -0,0 +1,13 @@
+﻿while (true)
+{
+    Console.Clear();
+    Console.WriteLine("***********DESAFIO 01 - CARNAVAL BALTA.IO***********\n");
+    Console.WriteLine("""Digite um texto (Para sair, basta digitar "exit"): """);
+    var inputText = Console.ReadLine();
+    if (inputText.ToLower() == "exit") Environment.Exit(0);
+    (int letterCount, int wordCount) = (inputText!.Length, inputText.Split(" ").Count());
+    Console.WriteLine($"{letterCount} caracteres, {wordCount} palavras");
+    Console.ReadKey();
+}
+
+
