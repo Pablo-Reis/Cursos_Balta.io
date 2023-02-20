090ff4d033b7a96d14f7eb1a3d17c5157d630717
Pablo Leite <pablo16072002@gmail.com>
2023-02-19 22:29:48 -0300

    Update 19/02/2023

---
+++ Desafios_Carnaval_Balta/Desafio3/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace Desafio3;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Clear();
+        try
+        {
+            Console.WriteLine("Qual combustível compensa mais? Preencha os campos abaixo para descobrir!");
+            Console.WriteLine("---------------------------------------------");
+            Console.Write("Valor do álcool: ");
+            decimal alcool = decimal.Parse(Console.ReadLine().Replace(',', '.'));
+            Console.Write("Valor da gasolina: ");
+            decimal gasolina = decimal.Parse(Console.ReadLine().Replace(',', '.'));
+            var relacao = alcool / gasolina;
+            Console.WriteLine($"A razão do preço do álcool para o preço da gasolina é {relacao.ToString("N2")}({relacao.ToString("P0")})");
+            Console.WriteLine((relacao * 100) <= 72 ? "Usar álcool é mais econômico!" : "Usar gasolina é mais econômico!");
+        }
+        catch (FormatException)
+        {
+            Console.WriteLine("Tipo incorreto de dado! Tente novamente..");
+            Console.ReadKey();
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine($"Ocorreu um erro durante a execução conforme a mensagem: '{e.Message}'");
+        }
+    }
+}
